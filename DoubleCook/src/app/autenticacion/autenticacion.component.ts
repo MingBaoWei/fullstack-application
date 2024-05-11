1836fd5884d9fe655702b874178e24560890d037
unknown <8NiceWei8@gmail.com>
2024-05-11 19:54:59 +0200

    NEW: DoubleCook y nuevo Register (autenticacion)

--- DoubleCook/src/app/autenticacion/autenticacion.component.ts
+++ DoubleCook/src/app/autenticacion/autenticacion.component.ts
@@ -1,32 +1,30 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { AuthService } from '../auth.service';
 
 @Component({
-  selector: 'app-registrar',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './registrar.component.html',
-  styleUrl: './registrar.component.css'
+  selector: 'app-autenticacion',
+  templateUrl: './autenticacion.component.html',
+  styleUrl: './autenticacion.component.css'
 })
-export class RegistrarComponent {
-  
+export class AutenticacionComponent {
+
   userData: any = {};
 
   constructor(private authService: AuthService) {}
 
-  registrarUsuario() {
+  registrarUsuario(event: any) {
+    event.preventDefault(); 
+    console.log('Registrando usuario...');
     this.authService.register(this.userData).subscribe(
       () => {
         console.log('Usuario registrado correctamente');
-        // Aquí puedes redirigir al usuario o mostrar un mensaje de éxito
       },
       (error) => {
         console.error('Error al registrar usuario:', error);
-        // Aquí puedes mostrar un mensaje de error al usuario
       }
     );
   }
+  
 
   showLoginForm: boolean = false;
   showRegisterForm: boolean = false;
@@ -40,4 +38,5 @@ export class RegistrarComponent {
     this.showLoginForm = false;
     this.showRegisterForm = true;
   }
+  
 }
