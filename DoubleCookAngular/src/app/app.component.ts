33d94873221caa703553cd322e36da46a8acc87d
Ernesto161199 <96296217+Ernesto161199@users.noreply.github.com>
2024-06-12 12:04:22 +0200

    compodonc_mejoraApi

--- DoubleCookAngular/src/app/app.component.ts
+++ DoubleCookAngular/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'DoubleCook';
+  constructor() { }
+
+  ngOnInit(): void {
+    const navToggle = document.querySelector('.nav-toggle');
+    const nav = document.querySelector('.nav');
+
+    navToggle?.addEventListener('click', () => {
+      nav?.classList.toggle('active');
+    });
+  }
 }
